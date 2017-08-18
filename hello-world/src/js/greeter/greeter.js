06252cfea31ea5cb179a116ee567f0b2f21e0ece
Peter Donald <peter@realityforge.org>
2017-08-18 21:10:13 +1000

    Add jsdoc annotations/comments

--- hello-world/src/js/greeter/greeter.js
+++ hello-world/src/js/greeter/greeter.js
@@ -2,6 +2,12 @@ goog.provide('rf.greeter');
 
 goog.require('goog.dom');
 
+/**
+ * Replace contents of element with specified id wit a greeting.
+ *
+ * @param {String} id the id of the dom element.
+ * @return {undefined}
+ */
 rf.greeter.greet = function(id) {
   var element = goog.dom.getElement(id);
   if (element) {
