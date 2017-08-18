8de3a3b30b05656ee1716c195daa1ea4bb451416
Peter Donald <peter@realityforge.org>
2017-08-18 21:09:14 +1000

    Namespace our code

--- hello-world/src/js/greeter/greeter.js
+++ hello-world/src/js/greeter/greeter.js
@@ -1,9 +1,11 @@
+goog.provide('rf.greeter');
+
 goog.require('goog.dom');
 
-function greet(id) {
+rf.greeter.greet = function(id) {
   var element = goog.dom.getElement(id);
   if (element) {
     element.innerHTML = 'Hello from closure library';
     console.log('Message printed.');
   }
-}
+};
