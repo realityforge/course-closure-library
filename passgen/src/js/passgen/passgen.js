bfab2717be692729970d4f55d7d59d9dbb3c2e0c
Peter Donald <peter@realityforge.org>
2017-08-19 07:28:29 +1000

    Initialize the basic passgen library

---
+++ passgen/src/js/passgen/passgen.js
@@ -0,0 +1,10 @@
+goog.provide('rf.passgen');
+
+/**
+ * Initialize passgen application with element identified by selector.
+ *
+ * @param {String} selector the selector of the dom element where application is initialized.
+ * @return {undefined}
+ */
+rf.passgen.init = function(selector) {
+};
