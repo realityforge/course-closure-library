cfda492636a48dcf66ce8835ef06ff2eed250fe0
Peter Donald <peter@realityforge.org>
2017-08-19 14:14:52 +1000

    Use closure compiler in simple mode

---
+++ greeter-cc/scripts/cc-simple.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const { exec } = require('child_process');
+var mkdirp = require('mkdirp');
+
+var CALC_DEPS_CMD = './node_modules/google-closure-library/closure/bin/calcdeps.py';
+var INPUT_PATH = process.argv[2];
+var INPUT_FILE = process.argv[3];
+var OUTPUT_PATH = process.argv[4];
+var OUTPUT_FILE = process.argv[5];
+
+let command = CALC_DEPS_CMD +
+              ' --compiler_jar ./node_modules/google-closure-compiler/compiler.jar' +
+              ' --path node_modules/google-closure-library/closure' +
+              ' --path ' + INPUT_PATH +
+              ' --input ' + INPUT_PATH + '/' + INPUT_FILE +
+              ' --compiler_flags "--compilation_level=SIMPLE_OPTIMIZATIONS"' +
+              ' --output_mode compiled' +
+              ' > ' + OUTPUT_PATH + '/' + OUTPUT_FILE;
+
+mkdirp.sync(OUTPUT_PATH);
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    process.exit(err);
+  } else {
+    console.log(stderr);
+  }
+});
