7d0ac8b2f2eeaa0bfb600d47a20601449dae444c
Peter Donald <peter@realityforge.org>
2017-08-19 13:50:37 +1000

    Update to javascript depsgen

---
+++ greeter-cc/scripts/depsgen.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { exec } = require('child_process');
+
+var DEPS_WRITER_CMD = './node_modules/google-closure-library/closure/bin/build/depswriter.py';
+var DIR = process.argv[2];
+
+let command = DEPS_WRITER_CMD +
+              ' --root_with_prefix="src/js/' + DIR + ' ../../../../src/js/' + DIR + '"' +
+              ' > src/js/' +
+              DIR + '/deps.js';
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    // node couldn't execute the command
+    process.exit(err);
+  } else {
+    console.log(stderr);
+  }
+});
