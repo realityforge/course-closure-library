a4c2d5f695798a3d2a108d101badc67df8445ac7
Peter Donald <peter@realityforge.org>
2017-08-20 15:01:30 +1000

    Implement storage service

---
+++ weather/src/js/weather/service/storage_service.js
@@ -0,0 +1,36 @@
+goog.provide('rf.weather.service.StorageService');
+
+goog.require('goog.storage.Storage');
+goog.require('goog.storage.mechanism.mechanismfactory');
+
+/**
+ * @constructor
+ * @returns {rf.weather.service.StorageService}
+ */
+rf.weather.service.StorageService = function() {
+  /** @private {null|goog.storage.Storage} */
+  this.storage_ = null;
+
+  var mechanism = goog.storage.mechanism.mechanismfactory.create('rf.weather');
+  if (mechanism) {
+    this.storage_ = new goog.storage.Storage(mechanism);
+  }
+};
+
+/**
+ * @param {string|null} value
+ */
+rf.weather.service.StorageService.prototype.setLocation = function(value) {
+  if (this.storage_) {
+    this.storage_.set('location', value);
+  }
+};
+
+/**
+ * @returns {string|null}
+ */
+rf.weather.service.StorageService.prototype.getLocation = function() {
+  if (this.storage_) {
+    return this.storage_.get('location');
+  }
+};
